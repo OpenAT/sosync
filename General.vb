6ed262c48b408c947c7c844d0600ca29d3ca36e2
thecro <seh@datadialog.net>
2016-02-26 13:53:36 +0100

    [ADD] prozesslogik komplett implementiert

---
+++ General.vb
@@ -0,0 +1,38 @@
+﻿Module General
+    Private Const sosync_executeable_name As String = "sosync.exe"
+    Public Sub file_create_or_replace(filename As String)
+        If Not System.IO.File.Exists(filename) Then
+            Dim str = System.IO.File.Create(filename)
+            str.Close()
+        End If
+    End Sub
+
+    Public Sub file_remove_if_exists(filename As String)
+        If System.IO.File.Exists(filename) Then
+            System.IO.File.Delete(filename)
+        End If
+    End Sub
+
+    Public Function file_exists(filename As String) As Boolean
+        Return System.IO.File.Exists(filename)
+    End Function
+
+    Public Function get_main_process_id(executing_directory As String) As Integer?
+
+        Dim numeric_files = (From el In (New System.IO.DirectoryInfo(executing_directory)).GetFiles() Where IsNumeric(el.Name) Select el).ToList()
+
+        If numeric_files.Count = 0 Then
+            Return Nothing
+        End If
+
+        If numeric_files.Count > 1 Then
+            'zustand darf nicht auftreten!!!
+        End If
+
+        If numeric_files.Count = 1 Then
+            Return Integer.Parse(numeric_files.FirstOrDefault().Name)
+        End If
+
+    End Function
+
+End Module
--- Main.vb
+++ Main.vb
@@ -2,6 +2,90 @@
 
     Sub Main()
 
+        Dim state As New sosyncState()
+        Dim hooks As New hooks(state)
+
+        If state.runlock Then
+            If state.update Then
+                state.unlink_main_process()
+                Exit Sub
+            ElseIf Not state.is_main_process Then
+                state.runagain = True
+                Exit Sub
+            End If
+        End If
+
+        state.runlock = True
+
+        If state.update Then
+            state.unlink_main_process()
+            hooks.update()
+            Exit Sub
+        End If
+
+        'andere blöcke
+sync_block:
+        sync_work()
+
+        If state.update Then
+            state.runagain = False
+            state.unlink_main_process()
+            hooks.update()
+            Exit Sub
+        End If
+
+        If state.runagain Then
+            state.runagain = False
+            GoTo sync_block
+        End If
+
+        state.runlock = False
+        state.unlink_main_process()
+        Exit Sub
+
+
+    End Sub
+
+    Private Sub sync_work()
+        System.Threading.Thread.Sleep(1000 * 10)
     End Sub
 
 End Module
+
+Module ConfigFileParser
+
+End Module
+
+Public Class ExecuteArgs
+
+
+    Public Property windows_user As String
+    Public Property windows_user_pw As String
+    Public Property odoo_user As String
+    Public Property odoo_user_pw As String
+    Public Property postgres_user As String
+    Public Property postgres_user_pw As String
+
+    Public Property postgres_connection_string As String
+
+    Public Property mssql_connection_string As String
+
+    Public Property odoo_api_url As String
+
+    Public Sub New()
+
+    End Sub
+
+
+
+End Class
+
+
+Public Class hooks
+    Public Sub New(state As sosyncState)
+
+    End Sub
+    Public Sub update()
+        'update hook call
+    End Sub
+End Class
