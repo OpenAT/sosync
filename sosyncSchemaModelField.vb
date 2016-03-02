0b996fb4fc6c5d11e275f2ee14a8e2454da7054d
thecro <seh@datadialog.net>
2016-03-02 15:14:59 +0100

    [IMP] types are not important anymore (auto-handling via .net-framework)

--- sosyncSchemaModelField.vb
+++ sosyncSchemaModelField.vb
@@ -1,8 +1,5 @@
 ﻿Public Class sosyncSchemaModelField
 
-    Public Property msSQLFieldName As String
-    Public Property pgSQLFieldName As String
-    Public Property msSQLType As String
-    Public Property pgSQLType As String
+    Public Property Name As String
 
 End Class
