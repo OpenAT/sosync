278688b63124bd4a8ab6051e34936d6ea8729f74
thecro <seh@datadialog.net>
2016-03-02 14:14:39 +0100

    [IMP] constructor defined

--- msSQLServer.vb
+++ msSQLServer.vb
@@ -1,3 +1,11 @@
 ﻿Public Class msSQLServer
 
+    Private _instance As String
+    Private _pw As String
+
+    Public Sub New(instance As String, pw As String)
+        Me._instance = instance
+        Me._pw = pw
+    End Sub
+
 End Class
