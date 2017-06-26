f555fc8ad8a6c8af054e58e6708bcd71756b4914
kaipm <kaipm@users.noreply.github.com>
2017-06-27 01:29:30 +0200

    [FIX] Added sync job transfer properties

--- Data/Models/SyncJobDto.cs
+++ Data/Models/SyncJobDto.cs
@@ -10,6 +10,9 @@ namespace WebSosync.Data.Models
     /// </summary>
     public class SyncJobDto
     {
-
+        public string Source_System { get; set; }
+        public string Source_Model { get; set; }
+        public string Source_Record_ID { get; set; }
+        public DateTime Job_Date { get; set; }
     }
 }
