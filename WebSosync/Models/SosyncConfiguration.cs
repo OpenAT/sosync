a3b76658ac5d6ee75d6a457e545b521c1aa27862
kaipm <kaipm@users.noreply.github.com>
2017-06-26 17:52:32 +0200

    [FIX] Added all configuration values

--- WebSosync/Models/SosyncConfiguration.cs
+++ WebSosync/Models/SosyncConfiguration.cs
@@ -1,11 +1,32 @@
 namespace WebSosync.Models
 {
+    /// <summary>
+    /// Represents the possible configuration options for sosync.
+    /// </summary>
     public class SosyncConfiguration
     {
-        public string Conf { get; set; }
+        /// <summary>
+        /// Port for Kestrel webserver.
+        /// </summary>
         public int Port { get; set; }
+
+        public string Instance { get; set; }
+
+        public string DB_Host { get; set; }
+        public int DB_Port { get; set; }
+        public string DB_Name { get; set; }
+        public string DB_User { get; set; }
+        public string DB_User_PW { get; set; }
 
         public string Log_File { get; set; }
         public string Log_Level { get; set; }
+
+        public string Studio_MSSQL_Host { get; set; }
+        public string Studio_Sosync_User { get; set; }
+        public string Studio_Sosync_PW { get; set; }
+
+        public string Online_Host { get; set; }
+        public string Online_Sosync_User { get; set; }
+        public string Online_Sosync_PW { get; set; }
     }
 }
