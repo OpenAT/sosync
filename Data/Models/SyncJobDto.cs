9be2495da1a697e453e92287bafb6ed4f759ade6
kaipm <kaipm@users.noreply.github.com>
2017-06-26 21:19:17 +0200

    [ADD] Prepared a sync job DTO object

---
+++ Data/Models/SyncJobDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSosync.Data.Models
+{
+    /// <summary>
+    /// Represents a sync job data transfer object (DTO) to be used
+    /// by webservice controllers.
+    /// </summary>
+    public class SyncJobDto
+    {
+
+    }
+}
