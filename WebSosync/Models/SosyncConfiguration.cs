5956e9a29ebf210f96cd4bf5e643d6106e3f2256
kaipm <kaipm@users.noreply.github.com>
2017-06-26 16:58:40 +0200

    [FIX] Removed alternative configuration switches for now

--- WebSosync/Models/SosyncConfiguration.cs
+++ WebSosync/Models/SosyncConfiguration.cs
@@ -3,11 +3,9 @@ namespace WebSosync.Models
     public class SosyncConfiguration
     {
         public string Conf { get; set; }
-        public string C => Conf;
-
         public int Port { get; set; }
-        public int P => Port;
 
         public string Log_File { get; set; }
+        public string Log_Level { get; set; }
     }
 }
