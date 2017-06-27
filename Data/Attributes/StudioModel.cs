6c475e08d3a0aa61f47acbb5b5c6e5d97b466e1b
kaipm <kaipm@users.noreply.github.com>
2017-06-27 08:45:11 +0200

    [ADD] Added attributes for decorating models with their corresponding system

---
+++ Data/Attributes/StudioModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSosync.Data
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class StudioModel : Attribute
+    {
+        public string Name { get; set; }
+
+        public StudioModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
