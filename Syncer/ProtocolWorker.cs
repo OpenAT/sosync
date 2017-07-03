1c3177009b5345db00b4379c7f09710e6c2dd8b3
kaipm <kaipm@users.noreply.github.com>
2017-07-03 10:45:20 +0200

    [CHORE] Minor changes in test code

--- Syncer/ProtocolWorker.cs
+++ Syncer/ProtocolWorker.cs
@@ -23,7 +23,7 @@ namespace Syncer
                 var client = new OdooClient($"http://{Configuration.Online_Host}/xmlrpc/2/", Configuration.Instance);
                 client.Authenticate(Configuration.Online_Sosync_User, Configuration.Online_Sosync_PW);
 
-                //var job = client.GetModel<SyncJob>("sosync.job", 1);
+                //var job = client.GetModel<SyncJob>("sosync.job", 18);
                 //int id = client.CreateModel<SyncJob>("sosync.job", jobs[0]);
                 //jobs[0].Job_Fso_ID = id;
             }
