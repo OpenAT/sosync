828dfd0586e094e2ff659fd0e4cfe94a1300d5e5
kaipm <kaipm@users.noreply.github.com>
2017-07-06 11:26:22 +0200

    [ADD] Added exception for missing attributes on classes

---
+++ Syncer/Exceptions/MissingAttributeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Exceptions
+{
+    public class MissingAttributeException : SyncerException
+    {
+        #region Constructors
+        public MissingAttributeException(string className, string attributeName)
+            : base($"The {attributeName} is required on the {className} class.")
+        { }
+        #endregion
+    }
+}
