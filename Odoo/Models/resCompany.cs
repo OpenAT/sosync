39fd2fc8b9605f2d854fe59ba8be796ed776c833
kaipm <kaipm@users.noreply.github.com>
2017-07-31 14:27:17 +0200

    [WIP] Added res.Company model, this is for testing mostly and might get removed again

---
+++ Odoo/Models/resCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Odoo.Models
+{
+    public class resCompany
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "partner_id")]
+        public string[] Partner { get; set; }
+    }
+}
