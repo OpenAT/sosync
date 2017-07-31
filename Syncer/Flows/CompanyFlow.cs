1ab378f23d30221d9bbedfb0a38f56962f8f95bf
kaipm <kaipm@users.noreply.github.com>
2017-07-31 14:30:42 +0200

    [WIP] Implemented Studio (create, update) and Online (update)

--- Syncer/Flows/CompanyFlow.cs
+++ Syncer/Flows/CompanyFlow.cs
@@ -7,6 +7,8 @@ using WebSosync.Data.Models;
 using dadi_data.Models;
 using System.Linq;
 using Syncer.Enumerations;
+using Odoo;
+using Odoo.Models;
 
 namespace Syncer.Flows
 {
@@ -60,19 +62,59 @@ namespace Syncer.Flows
                 acc = db.Read(new { xBPKAccountID = studioID }).SingleOrDefault();
             }
 
-            return new ModelInfo(studioID, acc.res_company_id, acc.write_date);
+            if (acc != null)
+                return new ModelInfo(studioID, acc.res_company_id, acc.write_date);
+
+            return null;
         }
 
         protected override void TransformToOnline(int studioID, TransformType action)
         {
-            // Do nothing, for now, to see if job gets closed successfully
-            //throw new NotImplementedException();
+            dboxBPKAccount acc = null;
+            using (var db = Mdb.GetDataService<dboxBPKAccount>())
+            {
+                acc = db.Read(new { xBPKAccountID = studioID }).SingleOrDefault();
+            }
+
+            if (action == TransformType.CreateNew)
+            {
+#warning TODO: implement CompanyFlow Online create
+            }
+            else
+            {
+                Odoo.Client.UpdateModel("res.company", new { name = acc.Name }, acc.res_company_id.Value);
+            }
         }
 
         protected override void TransformToStudio(int onlineID, TransformType action)
         {
-            // Do nothing, for now, to see if job gets closed successfully
-            //throw new NotImplementedException();
+            //var result = Odoo.Client.GetDictionary("res.company", onlineID, new string[] { "display_name" });
+            //var result = Odoo.Client.GetDictionary("res.company", onlineID, new string[] { });
+            var company = Odoo.Client.GetModel<resCompany>("res.company", onlineID);
+
+            using (var db = Mdb.GetDataService<dboxBPKAccount>())
+            {
+                if (action == TransformType.CreateNew)
+                {
+                    var entry = new dboxBPKAccount()
+                    {
+                        Name = company.Name,
+                        res_company_id = onlineID
+                    };
+
+                    db.Create(entry);
+                    Odoo.Client.UpdateModel("res.partner", new { sosync_fs_id = entry.xBPKAccountID }, Convert.ToInt32(company.Partner[0]));
+                }
+                else
+                {
+                    var dicPartner = Odoo.Client.GetDictionary("res.partner", Convert.ToInt32(company.Partner[0]), new string[] { "sosync_fs_id" });
+                    var sosync_fs_id = Convert.ToInt32(dicPartner["sosync_fs_id"]);
+
+                    var acc = db.Read(new { xBPKAccountID = sosync_fs_id }).SingleOrDefault();
+                    acc.Name = company.Name;
+                    db.Update(acc);
+                }
+            }
         }
         #endregion
 
