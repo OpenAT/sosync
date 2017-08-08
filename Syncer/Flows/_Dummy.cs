e3a13d89f51754efdda659febf6c4a7506a5d11c
kaipm <kaipm@users.noreply.github.com>
2017-08-08 10:25:31 +0200

    [WIP] Dummy class to simulate missing column in models while database schema is not updated

---
+++ Syncer/Flows/_Dummy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Syncer.Flows
+{
+    public class dboxBPKAccount : dadi_data.Models.dboxBPKAccount
+    {
+        [DataMember(Name="sosync_write_date")]
+        public DateTime? Sosync_Write_Date { get; set; }
+    }
+}
