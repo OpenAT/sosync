c5b2d82cdfdffcb45ae0177837a40769d6cd85a5
Martin Kaip <kaipm@users.noreply.github.com>
2017-08-28 14:53:19 +0200

    [IMP] Added Odoo data conversion class

---
+++ Odoo/Convert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odoo
+{
+    public static class Convert
+    {
+        public static int? ToInt32(string value)
+        {
+            if (Int32.TryParse(value, out int result))
+                return result;
+
+            return null;
+        }
+
+        public static DateTime? ToDateTime(string value)
+        {
+            DateTime? result = null;
+            if (value != "0")
+                result = DateTime.Parse(value);
+
+            return result;
+        }
+    }
+}
