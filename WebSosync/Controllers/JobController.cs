92a368d03f49993aac4e9c5382db2dfad3c2ac3a
Martin Kaip <kaipm@users.noreply.github.com>
2017-08-29 16:39:46 +0200

    [FIX] Don't set change date on creation

--- WebSosync/Controllers/JobController.cs
+++ WebSosync/Controllers/JobController.cs
@@ -87,7 +87,6 @@ namespace WebSosync.Controllers
                 // Defaults
                 job.Job_State = SosyncState.New;
                 job.Job_Fetched = DateTime.UtcNow;
-                job.Job_Last_Change = DateTime.UtcNow;
 
                 // Create the sync job, get it's ID into the result and start the job thread
                 db.CreateJob(job);
