6832af562155c7a9cf7a6b434401411e54eda6b5
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2017-09-05 16:47:01 +0200

    [IMP] Multi threaded testing including database updates in MSSQL

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using dadi_data;
+using dadi_data.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ManualTest
@@ -9,24 +13,45 @@ namespace ManualTest
     {
         static void Main(string[] args)
         {
+            // Give sosync2 server some time to start up
+            Thread.Sleep(5000);
 
             Console.WriteLine("Manual testing...");
-            var t = StartRequestsAsync();
-            t.Wait();
+
+            int count = 100;
+            var tasks = new Task[]
+            {
+                StartRequestsAsync("A", count),
+                StartRequestsAsync("B", count),
+                StartRequestsAsync("C", count),
+                StartRequestsAsync("D", count),
+                StartRequestsAsync("E", count)
+            };
+
+            Task.WhenAll(tasks);
+
             Console.WriteLine("Done!");
+            Console.ReadKey();
         }
 
-        static async Task StartRequestsAsync()
+        static async Task StartRequestsAsync(string identifier, int count)
         {
-            var count = 10;
-            string template = "http://localhost:5050/job/create?job_date={0:yyyy-MM-dd}%20{0:HH:mm:ss.fff}&job_source_system=fso&job_source_model=res.company&job_source_record_id=1";
+            string template = "http://localhost:5050/job/create?job_date={0:yyyy-MM-dd}%20{0:HH:mm:ss.fff}&job_source_system=fs&job_source_model=dbo.Person&job_source_record_id=13";
 
             var requests = new List<Task<WebResponse>>(count);
 
             for (int i = 0; i < count; i++)
             {
-                //Console.WriteLine(String.Format(template, DateTime.UtcNow));
-                Console.WriteLine($"Sending request {i + 1}...");
+                using (var db = new DataService<dboPerson>("Data Source=MSSQL1;Initial Catalog=mdb_demo; Integrated Security=True;"))
+                {
+                    db.BeginTransaction();
+                    var pers = db.Read(new { PersonID = 13 }).SingleOrDefault();
+                    pers.Name = $"Reynoldse-{identifier}-{i + 1}";
+                    db.Update(pers);
+                    db.CommitTransaction();
+                }
+
+                Console.WriteLine($"{identifier}: Sending request {i + 1}...");
                 var req = HttpWebRequest.Create(String.Format(template, DateTime.UtcNow));
                 req.Method = "GET";
                 requests.Add(req.GetResponseAsync());
