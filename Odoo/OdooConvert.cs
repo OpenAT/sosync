48a7ff79700e1b8533fcd7f07186be2f63d14986
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2017-09-11 16:01:45 +0200

    [IMP] Added string format for DateTime? fields

--- Odoo/OdooConvert.cs
+++ Odoo/OdooConvert.cs
@@ -28,5 +28,21 @@ namespace Odoo
 
             return result;
         }
+
+        /// <summary>
+        /// Converts a nullable date/time to a string with a fixed format,
+        /// or boolean false if it is null.
+        /// </summary>
+        /// <param name="value">The value to be converted.</param>
+        /// <returns></returns>
+        public static object ToStringOrBoolFalse(DateTime? value)
+        {
+            object result = (object)false;
+
+            if (value.HasValue)
+                result = value.Value.ToString("yyyy-MM-dd");
+
+            return result;
+        }
     }
 }
