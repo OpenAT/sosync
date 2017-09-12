bbb9238d093907fa89d85f653a5bf0301c92a966
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2017-09-12 20:55:47 +0200

    [WIP] Testing changes

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -18,64 +18,122 @@ namespace ManualTest
 
             Console.WriteLine("Manual testing...");
 
-            int count = 100;
-            var tasks = new Task[]
-            {
-                StartRequestsAsync("A", count),
-                StartRequestsAsync("B", count),
-                StartRequestsAsync("C", count),
-                StartRequestsAsync("D", count),
-                StartRequestsAsync("E", count)
-            };
+            //int count = 200; // 2000 Entries
+            //var tasks = new Task[]
+            //{
+            //    StartRequestsAsync("A", count),
+            //    StartRequestsAsync("B", count),
+            //    StartRequestsAsync("C", count),
+            //    StartRequestsAsync("D", count),
+            //    StartRequestsAsync("E", count),
+            //    StartRequestsAsync("F", count),
+            //    StartRequestsAsync("G", count),
+            //    StartRequestsAsync("H", count),
+            //    StartRequestsAsync("I", count),
+            //    StartRequestsAsync("J", count)
+            //};
 
-            Task.WaitAll(tasks);
+            int threadCount = 5;
+            int count = 100; // 5 x 100 = 500
+
+            for (int ti = 0; ti < threadCount; ti++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart((p) => 
+                {
+                    var pr = (ThreadParam)p;
+                    StartRequestsAsync(pr.ID, pr.Nr);
+                }));
+
+                t.Start(new ThreadParam() { ID = ((char)(65 + ti)).ToString(), Nr = count });
+            }
+
+            //StartRequestsAsync("A", count);
+            //StartRequestsAsync("B", count);
+            //StartRequestsAsync("C", count);
+            //StartRequestsAsync("D", count);
+            //StartRequestsAsync("E", count);
+            //StartRequestsAsync("F", count);
+            //StartRequestsAsync("G", count);
+            //StartRequestsAsync("H", count);
+            //StartRequestsAsync("I", count);
+            //StartRequestsAsync("J", count);
+
+            //Task.WhenAll(tasks);
 
             Console.WriteLine($"All tasks finished.");
             Console.ReadKey();
         }
 
-        static async Task StartRequestsAsync(string identifier, int count)
+        //static async Task StartRequestsAsync(string identifier, int count)
+        //{
+        //    //string template = "http://localhost:5050/job/create?job_date={0:yyyy-MM-dd}%20{0:HH:mm:ss.fffffff}&job_source_system=fs&job_source_model=dbo.Person&job_source_record_id=13&job_source_sosync_write_date={0:yyyy-MM-dd}T{0:HH:mm:ss.fffffff}Z";
+
+        //    var requests = new List<Task>(count);
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        var myId = identifier;
+        //        var myNr = i + 1;
+
+        //        var t = Task.Run(() =>
+        //        {
+        //            UpdateMssql(myId, myNr);
+        //        });
+
+        //        requests.Add(t);
+
+        //        // requests.Add(Task.Run(() => UpdateMssql(identifier, i + 1)));
+        //        //requests.Add(factory.StartNew(() => UpdateMssql(identifier, i + 1)));
+
+        //        //var req = HttpWebRequest.Create(String.Format(template, DateTime.UtcNow));
+        //        //req.Method = "GET";
+        //        //requests.Add(req.GetResponseAsync());
+        //    }
+        //    await Task.WhenAll(requests);
+        //}
+
+        static void StartRequestsAsync(string identifier, int count)
         {
             //string template = "http://localhost:5050/job/create?job_date={0:yyyy-MM-dd}%20{0:HH:mm:ss.fffffff}&job_source_system=fs&job_source_model=dbo.Person&job_source_record_id=13&job_source_sosync_write_date={0:yyyy-MM-dd}T{0:HH:mm:ss.fffffff}Z";
 
-            var requests = new List<Task>(count);
+            //var requests = new List<Task>(count);
 
-            for (int i = 0; i < count; i++)
+            using (var db = new DataService<dboPerson>("Data Source=MSSQL1;Initial Catalog=mdb_demo; Integrated Security=True;"))
             {
-                var myId = identifier;
-                var myNr = i + 1;
-
-                var t = Task.Run(() =>
+                for (int i = 0; i < count; i++)
                 {
-                    UpdateMssql(myId, myNr);
-                });
+                    UpdateMssql(identifier, i + 1, db);
 
-                requests.Add(t);
-                
-                // requests.Add(Task.Run(() => UpdateMssql(identifier, i + 1)));
-                //requests.Add(factory.StartNew(() => UpdateMssql(identifier, i + 1)));
+                    //var t = Task.Run(() =>
+                    //{
+                    //    UpdateMssql(myId, myNr);
+                    //});
 
-                //var req = HttpWebRequest.Create(String.Format(template, DateTime.UtcNow));
-                //req.Method = "GET";
-                //requests.Add(req.GetResponseAsync());
+                    //requests.Add(t);
+
+                    // requests.Add(Task.Run(() => UpdateMssql(identifier, i + 1)));
+                    //requests.Add(factory.StartNew(() => UpdateMssql(identifier, i + 1)));
+
+                    //var req = HttpWebRequest.Create(String.Format(template, DateTime.UtcNow));
+                    //req.Method = "GET";
+                    //requests.Add(req.GetResponseAsync());
+                }
             }
-            await Task.WhenAll(requests);
+            //await Task.WhenAll(requests);
         }
+
 
         private static Random _rnd = new Random();
 
-        private static void UpdateMssql(string id, int nr)
+        private static void UpdateMssql(string id, int nr, DataService<dboPerson> db)
         {
             Console.WriteLine($"{id}: Updating mssql {nr}...");
 
             try
             {
-                using (var db = new DataService<dboPerson>("Data Source=MSSQL1;Initial Catalog=mdb_demo; Integrated Security=True;"))
-                {
-                    var pers = db.Read(new { PersonID = 13 }).SingleOrDefault();
-                    pers.Name = $"Reynoldse-{id}-{nr}";
-                    db.Update(pers);
-                }
+                var pers = db.Read(new { PersonID = 13 }).SingleOrDefault();
+                pers.Name = $"Reynoldse-{id}-{nr}";
+                db.Update(pers);
 
                 Console.WriteLine($"{id}: {nr} is done...");
             }
---
+++ ManualTest/ThreadParam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManualTest
+{
+    public class ThreadParam
+    {
+        public string ID { get; set; }
+        public int Nr { get; set; }
+    }
+}
