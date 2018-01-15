cf459a32c4c6e09293cddda8cd02110e2f69c7ed
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-01-15 16:58:53 +0100

    [WIP] Prepared field class for unified sync flow fields

---
+++ Syncer/Workers/UniformFieldMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Workers
+{
+    public class UniformFieldMap
+    {
+        public string FsoField { get; set; }
+        public string FsField { get; set; }
+    }
+}
