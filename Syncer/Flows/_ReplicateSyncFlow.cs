4f19cb0413011fba51b9fa87b0d4d5ea63bd8154
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-01-31 21:36:35 +0100

    [FIX] Times are already utc

--- Syncer/Flows/_ReplicateSyncFlow.cs
+++ Syncer/Flows/_ReplicateSyncFlow.cs
@@ -207,7 +207,7 @@ namespace Syncer.Flows
         private TimeSpan GetWriteDateDifference(string anyModelName, ModelInfo studioInfo, ModelInfo onlineInfo, int toleranceMS)
         {
             var onlineWriteDate = onlineInfo.SosyncWriteDate ?? onlineInfo.WriteDate.Value;
-            var studioWriteDate = (studioInfo.SosyncWriteDate ?? studioInfo.WriteDate.Value.ToUniversalTime());
+            var studioWriteDate = studioInfo.SosyncWriteDate ?? studioInfo.WriteDate.Value;
 
             var result = onlineWriteDate - studioWriteDate;
 
