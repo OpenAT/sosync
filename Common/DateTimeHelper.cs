06dea0dcb4f1ca995ca169f8fc04bdbe1b2ba5f0
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-02-28 17:35:24 +0100

    [IMP] Helper class for conversion of nullable DateTime fields

---
+++ Common/DateTimeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSosync.Common
+{
+    public static class DateTimeHelper
+    {
+        public static DateTime? ToUtc(DateTime? localTime)
+        {
+            if (localTime.HasValue)
+                return localTime.Value.ToUniversalTime();
+
+            return null;
+        }
+
+        public static DateTime? ToLocal(DateTime? universalTime)
+        {
+            if (universalTime.HasValue)
+                return universalTime.Value.ToLocalTime();
+
+            return null;
+        }
+    }
+}
