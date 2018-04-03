a897566441cad6cdd566511e56b036b094620553
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-04-03 14:22:26 +0200

    [FIX] Do not throw exception, if this optimization cannot be run

--- Syncer/Workers/SyncWorker.cs
+++ Syncer/Workers/SyncWorker.cs
@@ -209,7 +209,7 @@ namespace Syncer.Workers
         private void CloseAllPreviousJobs(SyncJob job)
         {
             if (!job.Job_Source_Sosync_Write_Date.HasValue)
-                throw new SyncerException($"Submitted {nameof(job.Job_Source_Sosync_Write_Date)} was null, cannot close previous jobs (job_id = {job.Job_ID})");
+                return;
 
             using (var db = GetDb())
             {
