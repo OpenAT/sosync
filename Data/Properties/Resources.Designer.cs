eda04f1033f5570ddc01c9dbfa197b9e38cc6ae4
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-04-03 13:43:24 +0200

    [FIX] Increase job_date limit in preparation for re-processing error jobs

--- Data/Properties/Resources.Designer.cs
+++ Data/Properties/Resources.Designer.cs
@@ -138,7 +138,7 @@ namespace WebSosync.Data.Properties {
         ///	select * from (
         ///		select *
         ///		from sync_table
-        ///		where job_date &gt; now() - interval &apos;15 days&apos; and parent_job_id is null and job_state = &apos;new&apos;
+        ///		where job_date &gt; now() - interval &apos;100 days&apos; and parent_job_id is null and job_state = &apos;new&apos;
         ///        order by job_date desc
         ///        limit 50
         ///	) first_parent
