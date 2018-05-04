699f346873ed67868b34e4ca539d35446a7eeccc
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-05-04 11:37:12 +0200

    [ADD] Add new attribute for model priority handling

---
+++ Syncer/Attributes/ModelPriorityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ModelPriorityAttribute : Attribute
+    {
+        public ModelPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; set; }
+    }
+}
