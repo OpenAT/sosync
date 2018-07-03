a26265383ab76e4ddf476ab629dbdac235f6bc0e
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-07-03 15:05:49 +0200

    [FIX] Update data-layer [WIP] Prepare modles for zGruppe

---
+++ Syncer/Flows/zGruppeFlow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Models;
+using WebSosync.Data.Models;
+
+namespace Syncer.Flows
+{
+    [StudioModel(Name = "dbo.zGruppe")]
+    [OnlineModel(Name = "")]
+    public class zGruppeFlow
+        : ReplicateSyncFlow
+    {
+        public zGruppeFlow(IServiceProvider svc, SosyncOptions conf) : base(svc, conf)
+        {
+        }
+
+        protected override ModelInfo GetOnlineInfo(int onlineID)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ModelInfo GetStudioInfo(int studioID)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetupOnlineToStudioChildJobs(int onlineID)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SetupStudioToOnlineChildJobs(int studioID)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
