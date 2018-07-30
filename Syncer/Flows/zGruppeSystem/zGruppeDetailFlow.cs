4471afad8a3181ac130f004d30a7424a5c6fa787
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-07-30 18:21:36 +0200

    [FIX] Use correct group id, restrict sync direction from FS -> FSO only

--- Syncer/Flows/zGruppeSystem/zGruppeDetailFlow.cs
+++ Syncer/Flows/zGruppeSystem/zGruppeDetailFlow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DaDi.Odoo;
+using DaDi.Odoo.Models;
 using dadi_data.Models;
 using Syncer.Attributes;
 using Syncer.Enumerations;
@@ -30,32 +31,75 @@ namespace Syncer.Flows.zGruppeSystem
 
         protected override void SetupStudioToOnlineChildJobs(int studioID)
         {
-            using (var db = MdbService.GetDataService<dbozGruppe>())
+            using (var db = MdbService.GetDataService<dbozGruppeDetail>())
             {
-                var studioModel = db.Read(new { zGruppeID = studioID }).SingleOrDefault();
-                RequestChildJob(SosyncSystem.FundraisingStudio, StudioModelName, studioModel.zGruppeID);
+                var studioModel = db.Read(new { zGruppeDetailID = studioID }).SingleOrDefault();
+                RequestChildJob(SosyncSystem.FundraisingStudio, "dbo.zGruppe", studioModel.zGruppeID);
             }
         }
 
         protected override void SetupOnlineToStudioChildJobs(int onlineID)
         {
             var odooModel = OdooService.Client.GetDictionary(OnlineModelName, onlineID, new string[] { "zgruppe_id" });
-            var frstzGruppeID = OdooConvert.ToInt32((string)((List<object>)odooModel["zgruppe_id"])[0]);
+            var odooGruppeID = OdooConvert.ToInt32((string)((List<object>)odooModel["zgruppe_id"])[0]);
 
-            RequestChildJob(SosyncSystem.FSOnline, "frst.zgruppe", frstzGruppeID.Value);
+            RequestChildJob(SosyncSystem.FSOnline, "frst.zgruppe", odooGruppeID.Value);
         }
 
         protected override void TransformToOnline(int studioID, TransformType action)
         {
-            throw new NotImplementedException();
+            var odoozgruppeID = 0;
+            dbozGruppeDetail grpDetail = null;
+            using (var db = MdbService.GetDataService<dbozGruppeDetail>())
+            {
+                grpDetail = db.Read(new { zGruppeDetailID = studioID }).SingleOrDefault();
+
+                if (!grpDetail.sosync_fso_id.HasValue)
+                    grpDetail.sosync_fso_id = GetFsoIdByFsId(OnlineModelName, grpDetail.zGruppeDetailID);
+
+                odoozgruppeID = GetFsoIdByFsId("frst.zgruppe", grpDetail.zGruppeID).Value;
+            }
+
+            SimpleTransformToOnline<dbozGruppeDetail, frstzGruppedetail>(
+                studioID,
+                action,
+                studioModel => studioModel.zGruppeDetailID,
+                (studio, online) =>
+                    {
+                        online.Add("zgruppe_id", Convert.ToString(odoozgruppeID));
+                        online.Add("gruppe_kurz", studio.GruppeKurz);
+                        online.Add("gruppe_lang", studio.GruppeLang);
+                        online.Add("gui_anzeigen", studio.GUIAnzeigen);
+                        online.Add("gueltig_von", studio.GültigVon.Date);
+                        online.Add("gueltig_bis", studio.GültigBis.Date);
+                    });
         }
 
         protected override void TransformToStudio(int onlineID, TransformType action)
         {
-            if (action == TransformType.CreateNew)
-                throw new SyncerException($"{StudioModelName} can only be created from FS, not from FS-Online.");
+            throw new SyncerException($"{StudioModelName} can only be created/updated from FS, not from FS-Online.");
 
-            throw new NotImplementedException();
+            //if (action == TransformType.CreateNew)
+            //    throw new SyncerException($"{StudioModelName} can only be created from FS, not from FS-Online.");
+
+            //var odooGrp = OdooService.Client.GetModel<frstzGruppedetail>(OnlineModelName, onlineID);
+
+            //if (!IsValidFsID(odooGrp.Sosync_FS_ID))
+            //    odooGrp.Sosync_FS_ID = GetFsIdByFsoId(StudioModelName, MdbService.GetStudioModelIdentity(StudioModelName), onlineID);
+
+            //var zGruppeID = GetFsIdByFsoId("dbo.zGruppe", "zGruppeID", odooGrp.zgruppe_id).Value;
+
+            //SimpleTransformToStudio<frstzGruppedetail, dbozGruppeDetail>(
+            //    onlineID,
+            //    action,
+            //    studioModel => studioModel.zGruppeDetailID,
+            //    (online, studio) =>
+            //        {
+            //            studio.zGruppeID = zGruppeID;
+            //            studio.GruppeKurz = online.gruppe_kurz;
+            //            studio.GruppeLang = online.gruppe_lang;
+            //            studio.GUIAnzeigen = online.gui_anzeigen;
+            //        });
         }
     }
 }
--- Syncer/Flows/zGruppeSystem/zGruppeFlow.cs
+++ Syncer/Flows/zGruppeSystem/zGruppeFlow.cs
@@ -44,22 +44,24 @@ namespace Syncer.Flows.zGruppeSystem
 
         protected override void TransformToStudio(int onlineID, TransformType action)
         {
-            // zGruppe originates from a central database, so there are special steps
-            // required to create a new one.
-            if (action == TransformType.CreateNew)
-                throw new SyncerException($"{StudioModelName} can only be created from FS, not from FS-Online.");
+            throw new SyncerException($"{StudioModelName} can only be created/updated from FS, not from FS-Online.");
 
-            SimpleTransformToStudio<frstzGruppe, dbozGruppe>(
-                onlineID,
-                action,
-                studioModel => studioModel.zGruppeID,
-                (online, studio) =>
-                    {
-                        studio.TabellentypID = online.tabellentyp_id;
-                        studio.GruppeKurz = online.gruppe_kurz;
-                        studio.GruppeLang = online.gruppe_lang;
-                        studio.GUIAnzeigen = online.gui_anzeigen;
-                    });
+            //// zGruppe originates from a central database, so there are special steps
+            //// required to create a new one.
+            //if (action == TransformType.CreateNew)
+            //    throw new SyncerException($"{StudioModelName} can only be created from FS, not from FS-Online.");
+
+            //SimpleTransformToStudio<frstzGruppe, dbozGruppe>(
+            //    onlineID,
+            //    action,
+            //    studioModel => studioModel.zGruppeID,
+            //    (online, studio) =>
+            //        {
+            //            studio.TabellentypID = online.tabellentyp_id;
+            //            studio.GruppeKurz = online.gruppe_kurz;
+            //            studio.GruppeLang = online.gruppe_lang;
+            //            studio.GUIAnzeigen = online.gui_anzeigen;
+            //        });
         }
     }
 }
