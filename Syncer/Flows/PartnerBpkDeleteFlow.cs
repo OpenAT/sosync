169e82092f09bd8b7fa1ae284632aea6a11a6f51
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-08-01 13:04:19 +0200

    [CHORE] Refactor to new delete methods, remove unused methods

--- Syncer/Flows/PartnerBpkDeleteFlow.cs
+++ Syncer/Flows/PartnerBpkDeleteFlow.cs
@@ -19,16 +19,6 @@ namespace Syncer.Flows
         {
         }
 
-        protected override void SetupOnlineToStudioChildJobs(int onlineID)
-        {
-            // No child jobs for delete
-        }
-
-        protected override void SetupStudioToOnlineChildJobs(int studioID)
-        {
-            // No child jobs for delete
-        }
-
         protected override void TransformToOnline(int studioID, TransformType action)
         {
             throw new NotSupportedException($"Delete from [fs] to [fso] for model {StudioModelName}.");
@@ -36,24 +26,7 @@ namespace Syncer.Flows
 
         protected override void TransformToStudio(int onlineID, TransformType action)
         {
-            using (var db = MdbService.GetDataService<dboPersonBPK>())
-            {
-                var data = db.Read(new { PersonBPKID = Job.Sync_Target_Record_ID.Value }).SingleOrDefault();
-
-                if (data == null)
-                    throw new SyncerException($"Failed to read data from model {StudioModelName} {Job.Sync_Target_Record_ID.Value} before deletion.");
-
-                UpdateSyncTargetDataBeforeUpdate(Serializer.ToXML(data));
-
-                var query = $"delete from {StudioModelName} where {MdbService.GetStudioModelIdentity(StudioModelName)} = @id; select @@ROWCOUNT;";
-                UpdateSyncTargetRequest($"-- @id = {Job.Sync_Target_Record_ID.Value}\n" + query);
-
-                var affectedRows = db.ExecuteQuery<int>(query, new { id = Job.Sync_Target_Record_ID.Value }).SingleOrDefault();
-                UpdateSyncTargetAnswer($"Deleted rows: {affectedRows}", null);
-
-                if (affectedRows == 0)
-                    throw new SyncerException($"Failed to delete model {StudioModelName} {Job.Sync_Target_Record_ID.Value}, no rows affected by the delete.");
-            }
+            SimpleDeleteInStudio<dboPersonBPK>(onlineID);
         }
     }
 }
