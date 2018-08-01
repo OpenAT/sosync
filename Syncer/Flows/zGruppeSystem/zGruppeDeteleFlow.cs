fc47559b29c3c2bbc68ae064a526052207395344
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-08-01 13:01:24 +0200

    [CHORE] Refactor delete flow to new methods, remove comments and unused methods

--- Syncer/Flows/zGruppeSystem/zGruppeDeteleFlow.cs
+++ Syncer/Flows/zGruppeSystem/zGruppeDeteleFlow.cs
@@ -21,61 +21,14 @@ namespace Syncer.Flows.zGruppeSystem
         {
         }
 
-        protected override void SetupOnlineToStudioChildJobs(int onlineID)
-        {
-            // No child jobs for delete
-        }
-
-        protected override void SetupStudioToOnlineChildJobs(int studioID)
-        {
-            // No child jobs for delete
-        }
-
         protected override void TransformToOnline(int studioID, TransformType action)
         {
-            var id = 0;
-
-            if (Job.Job_Source_Target_Record_ID.HasValue && Job.Job_Source_Target_Record_ID > 0)
-                id = Job.Job_Source_Target_Record_ID.Value;
-            else
-                id = GetOnlineIDFromOdooViaStudioID(OnlineModelName, Job.Job_Source_Record_ID).Value;
-
-            var data = OdooService.Client.GetModel<frstzGruppe>(OnlineModelName, id);
-
-            if (data == null)
-                throw new SyncerException($"Failed to read data from model {OnlineModelName} {Job.Sync_Target_Record_ID.Value} before deletion.");
-
-            UpdateSyncTargetDataBeforeUpdate(OdooService.Client.LastResponseRaw);
-
-            OdooService.Client.UnlinkModel(OnlineModelName, id);
+            SimpleDeleteInOnline<frstzGruppe>(studioID);
         }
 
         protected override void TransformToStudio(int onlineID, TransformType action)
         {
-            // zGruppe cannot be deleted. GUIAnzeigen = false should be used instead.
             throw new SyncerException($"Model {StudioModelName} can only be deleted from FS, not from FS-Online.");
-
-            /*
-            using (var db = MdbService.GetDataService<dbozGruppe>())
-            {
-                var data = db.Read(new { zGruppeID = Job.Sync_Target_Record_ID.Value })
-                    .SingleOrDefault();
-
-                if (data == null)
-                    throw new SyncerException($"Failed to read data from model {StudioModelName} {Job.Sync_Target_Record_ID.Value} before deletion.");
-
-                UpdateSyncTargetDataBeforeUpdate(Serializer.ToXML(data));
-
-                var query = $"delete from {StudioModelName} where {MdbService.GetStudioModelIdentity(StudioModelName)} = @id; select @@ROWCOUNT;";
-                UpdateSyncTargetRequest($"-- @id = {Job.Sync_Target_Record_ID.Value}\n" + query);
-
-                var affectedRows = db.ExecuteQuery<int>(query, new { id = Job.Sync_Target_Record_ID.Value }).SingleOrDefault();
-                UpdateSyncTargetAnswer($"Deleted rows: {affectedRows}", null);
-
-                if (affectedRows == 0)
-                    throw new SyncerException($"Failed to delete model {StudioModelName} {Job.Sync_Target_Record_ID.Value}, no rows affected by the delete.");
-            }
-            */
         }
     }
 }
