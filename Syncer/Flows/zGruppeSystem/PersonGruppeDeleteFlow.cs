28bc31ccee0469a632a52852dc57aa12a3ec291d
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-08-01 12:55:41 +0200

    [ADD] Add delete flow

---
+++ Syncer/Flows/zGruppeSystem/PersonGruppeDeleteFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DaDi.Odoo.Models;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using WebSosync.Data.Models;
+
+namespace Syncer.Flows.zGruppeSystem
+{
+    [StudioModel(Name = "dbo.PersonGruppe")]
+    [OnlineModel(Name = "frst.persongruppe")]
+    public class PersonGruppeDeleteFlow
+        : DeleteSyncFlow
+    {
+        public PersonGruppeDeleteFlow(IServiceProvider svc, SosyncOptions conf) : base(svc, conf)
+        {
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleDeleteInOnline<frstPersongruppe>(studioID);
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleDeleteInStudio<dboPersonGruppe>(onlineID);
+        }
+    }
+}
