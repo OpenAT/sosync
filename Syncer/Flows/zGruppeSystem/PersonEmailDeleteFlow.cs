692122a7c181f10d5174bebab2dba3721b429bf0
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-08-13 18:10:14 +0200

    [ADD] Add email delete flow

---
+++ Syncer/Flows/zGruppeSystem/PersonEmailDeleteFlow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DaDi.Odoo.Models;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using WebSosync.Data.Models;
+
+namespace Syncer.Flows.zGruppeSystem
+{
+    [StudioModel(Name = "dbo.PersonEmail")]
+    [OnlineModel(Name = "frst.personemail")]
+    public class PersonEmailDeleteFlow
+        : DeleteSyncFlow
+    {
+        public PersonEmailDeleteFlow(IServiceProvider svc, SosyncOptions conf)
+            : base(svc, conf)
+        {
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleDeleteInOnline<frstPersonemail>(studioID);
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleDeleteInStudio<dboPersonEmail>(onlineID);
+        }
+    }
+}
