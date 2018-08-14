2d80f42bcd072547c6943b2af68a8a22d682e385
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2018-08-14 13:09:32 +0200

    [ADD] Add and implement PersonEmailGruppe flows

---
+++ Syncer/Flows/zGruppeSystem/PersonEmailGruppeDeleteFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DaDi.Odoo.Models;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using WebSosync.Data.Models;
+
+namespace Syncer.Flows.zGruppeSystem
+{
+    [StudioModel(Name = "dbo.PersonEmailGruppe")]
+    [OnlineModel(Name = "frst.personemailgruppe")]
+    public class PersonEmailGruppeDeleteFlow
+        : DeleteSyncFlow
+    {
+        public PersonEmailGruppeDeleteFlow(IServiceProvider svc, SosyncOptions conf) : base(svc, conf)
+        {
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleDeleteInOnline<frstPersonemailgruppe>(studioID);
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleDeleteInStudio<dboPersonEmailGruppe>(onlineID);
+        }
+    }
+}
