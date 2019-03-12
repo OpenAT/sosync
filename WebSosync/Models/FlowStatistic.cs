04dccd90ac2cca039516eaf19aed141658b4eeb2
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2019-03-12 09:21:40 +0100

    [IMP] Add online queue and flow statisics

--- WebSosync/Models/FlowStatistic.cs
+++ WebSosync/Models/FlowStatistic.cs
@@ -7,6 +7,11 @@ namespace WebSosync.Models
 {
     public class FlowStatistic
     {
+        public FlowStatistic()
+        {
+            UnsynchronizedModels = new Dictionary<string, int>();
+        }
+
         public Dictionary<string, int> UnsynchronizedModels { get; set; }
     }
 }
