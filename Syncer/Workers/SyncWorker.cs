cae4155460c53238a3eed59608ee3306dd0a9afe
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2019-04-11 20:09:02 +0200

    [IMP] Remove IListExtensions, use new nuget package instead

--- Syncer/Workers/SyncWorker.cs
+++ Syncer/Workers/SyncWorker.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Studio.Utility.Extensions;
 using Syncer.Exceptions;
 using Syncer.Flows;
 using Syncer.Helpers;
