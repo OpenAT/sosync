e0e586a82c5d48a6823692f1dfcb601f79f155e5
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2019-12-18 14:55:20 +0100

    [IMP] Check address block during transformation

---
+++ Syncer/Exceptions/AddressBlockMissingException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Exceptions
+{
+    public class AddressBlockMissingException : SyncerException
+    {
+    }
+}
