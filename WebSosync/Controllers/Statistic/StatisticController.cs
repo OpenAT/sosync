7a7aae22ef0f17b6abbe60f3096c6a175b877194
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-01-15 15:32:51 +0100

    [IMP] Add statistic controller for sosync statistics

---
+++ WebSosync/Controllers/Statistic/StatisticController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebSosync.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSosync.Controllers.Statistic
+{
+    [Route("statistic")]
+    public class StatisticController : Controller
+    {
+        private DataService _db;
+
+        public StatisticController(DataService db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("jobs")]
+        public async Task<IActionResult> Jobs()
+        {
+            var rows = (await _db.Connection
+                .QueryAsync("select job_state, job_source_model, count(*) count from sosync_job where parent_job_id is null group by job_state, job_source_model order by job_state, job_source_model"))
+                .ToArray();
+
+            return new OkObjectResult(rows);
+        }
+    }
+}
