64397ea0236040e269bd326584e6ea706108ca9a
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-04-27 16:17:42 +0200

    [IMP] Add more model validation

--- WebSosync/Models/JobDto.cs
+++ WebSosync/Models/JobDto.cs
@@ -8,22 +8,36 @@ namespace WebSosync.Models
     {
         [Required]
         public DateTime job_date { get; set; }
+        
         [Required]
+        [RegularExpression("^(fs|fso)$")]
         public string job_source_system { get; set; }
+        
         [Required]
         public string job_source_model { get; set; }
+        
         [Required]
+        [Range(1, int.MaxValue)]
         public int job_source_record_id { get; set; }
+        
+        [RegularExpression("^(delete|merge_into)$")]
         public string job_source_type { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int? job_source_merge_into_record_id { get; set; }
+        
+        [Range(1, int.MaxValue)]
         public int? job_source_target_merge_into_record_id { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int? job_source_target_record_id { get; set; }
 
         [Required]
         public DateTime job_source_sosync_write_date { get; set; }
+        
         public Dictionary<string, string> job_source_fields { get; set; }
+        
+        [Range(0, int.MaxValue)]
         public int? job_priority { get; set; }
 
         public JobDto[] children { get; set; }
