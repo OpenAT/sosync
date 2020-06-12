0e2a6280903dcc2bd79e483b94cb976ec5827135
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-06-12 09:38:21 +0200

    [IMP] Add new exception for cleanup errors

---
+++ Syncer/Exceptions/SyncCleanupException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Exceptions
+{
+    public class SyncCleanupException
+        : SyncerException
+    {
+        public SyncCleanupException()
+        { }
+
+        public SyncCleanupException(string message)
+            : base(message)
+        { }
+
+        public SyncCleanupException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
