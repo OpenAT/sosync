db333231b154dd15d46c459a15c27d0abfac9ae3
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-06-12 09:43:46 +0200

    [IMP] Wrap child job exceptions in custom exception

---
+++ Syncer/Exceptions/ChildJobException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Exceptions
+{
+    public class ChildJobException
+        : SyncerException
+    {
+        public ChildJobException()
+        { }
+
+        public ChildJobException(string message) : base(message)
+        { }
+
+        public ChildJobException(string message, Exception innerException) : base(message, innerException)
+        { }
+    }
+}
