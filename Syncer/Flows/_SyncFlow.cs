96daf067f1e49408b3133e28a2d8aa19de4339dd
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-06-16 15:13:30 +0200

    [FIX] Throw exception on both error and error_retry child job state

--- Syncer/Flows/_SyncFlow.cs
+++ Syncer/Flows/_SyncFlow.cs
@@ -470,7 +470,7 @@ namespace Syncer.Flows
                     {
                         foreach (var childJob in existingChildren)
                         {
-                            if (childJob.Job_State == SosyncState.Error.Value)
+                            if (childJob.Job_State == SosyncState.Error.Value || childJob.Job_State == SosyncState.ErrorRetry.Value)
                                 throw new SyncerException($"{childDescription} ({childJob.ID}) for [{childJob.Job_Source_System}] {childJob.Job_Source_Model} ({childJob.Job_Source_Record_ID}) failed.");
 
                             if (childJob.Job_State == SosyncState.New.Value || childJob.Job_State == SosyncState.InProgress.Value)
@@ -564,7 +564,7 @@ namespace Syncer.Flows
                             }
                         }
 
-                        if (entry.Job_State == SosyncState.Error.Value)
+                        if (entry.Job_State == SosyncState.Error.Value || entry.Job_State == SosyncState.ErrorRetry.Value)
                             throw new SyncerException($"{childDescription} ({entry.ID}) for [{entry.Job_Source_System}] {entry.Job_Source_Model} ({entry.Job_Source_Record_ID}) failed.");
 
                         // If the job is new or marked as "in progress", run it
