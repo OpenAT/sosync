48465a379664ab84a3f737f2deae373025222b63
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-07-01 14:43:15 +0200

    [IMP] Delete donation reports from FS to FSO only

---
+++ Syncer/Flows/PartnerDonationReportDeleteFlow.cs
@@ -0,0 +1,30 @@
+﻿using DaDi.Odoo.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syncer.Flows
+{
+    [StudioModel(Name = "dbo.AktionSpendenmeldungBPK")]
+    [OnlineModel(Name = "res.partner.donation_report")]
+    public class PartnerDonationReportDeleteFlow
+        : DeleteSyncFlow
+    {
+        public PartnerDonationReportDeleteFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleDeleteInOnline<resPartnerDonationReport>(studioID);
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            throw new NotSupportedException("Donation report can only be deleted from FS to FSO");
+        }
+    }
+}
