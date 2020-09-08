9608b057838a03b8e0aac07d3d1cb0cd298b211a
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2020-09-08 10:04:59 +0200

    [IMP] Oneway sync AnredeLang

--- Syncer/Flows/PersonEmailFlow.cs
+++ Syncer/Flows/PersonEmailFlow.cs
@@ -113,12 +113,16 @@ namespace Syncer.Flows
                 odooPartnerID)
                 .Value;
 
+            dboPersonEmail studioMail = null;
+
             SimpleTransformToStudio<frstPersonemail, dboPersonEmail>(
                 onlineID,
                 action,
                 studioModel => studioModel.PersonEmailID,
                 (online, studio) =>
                 {
+                    studioMail = studio;
+
                     EmailHelper.SplitEmail(online.email, out var mailVor, out var mailNach);
 
                     studio.PersonID = PersonID;
@@ -136,6 +140,14 @@ namespace Syncer.Flows
                     studio.BestaetigungsHerkunft = online.bestaetigt_herkunft;
                     // studio.AnredeLang // Do not set
                 });
+
+            // Studio will calculate AnredeLang, so we always need that update back
+            RequestPostTransformChildJob(
+                SosyncSystem.FundraisingStudio,
+                StudioModelName,
+                studioMail.PersonEmailID,
+                false,
+                SosyncJobSourceType.Default);
         }
     }
 }
