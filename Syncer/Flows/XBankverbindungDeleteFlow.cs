48f1ea69b022fc6c914bf3268636193e7873e7f3
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-02-21 09:52:33 +0100

    Add xBankverbindung sync and delete flows

---
+++ Syncer/Flows/XBankverbindungDeleteFlow.cs
@@ -0,0 +1,29 @@
+﻿using DaDi.Odoo.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Services;
+using System;
+
+namespace Syncer.Flows
+{
+    [StudioModel(Name = "dbo.xBankverbindung")]
+    [OnlineModel(Name = "frst.xbankverbindung")]
+    public class XBankverbindungDeleteFlow
+        : DeleteSyncFlow
+    {
+        public XBankverbindungDeleteFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleDeleteInOnline<frstxBankverbindung>(studioID);
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            // Kein sync zu FS
+            throw new NotSupportedException("");
+        }
+    }
+}
---
+++ Syncer/Flows/XBankverbindungFlow.cs
@@ -0,0 +1,47 @@
+﻿using DaDi.Odoo.Models;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Models;
+using Syncer.Services;
+using System;
+
+namespace Syncer.Flows
+{
+    [StudioModel(Name = "dbo.xBankverbindung")]
+    [OnlineModel(Name = "frst.xbankverbindung")]
+    public class XBankverbindungFlow
+        : ReplicateSyncFlow
+    {
+        public XBankverbindungFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override ModelInfo GetStudioInfo(int studioID)
+        {
+            return GetDefaultStudioModelInfo<dboxBankverbindung>(studioID);
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            SimpleTransformToOnline<dboxBankverbindung, frstxBankverbindung>(
+                studioID,
+                action,
+                studio => studio.xBankverbindungID,
+                (studio, online) =>
+                {
+                    online.Add("beschreibung", studio.Beschreibung);
+                    online.Add("kurzbezeichnung", studio.Kurzbezeichnung);
+                    online.Add("bankleitzahl", studio.Bankleitzahl);
+                    online.Add("kontonummer", studio.Kontonummer);
+                    online.Add("xiban", studio.xIBAN);
+                });
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            // Kein sync zu FS
+            throw new NotSupportedException();
+        }
+    }
+}
