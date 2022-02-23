733fa55793427dfd16f20febeb506d3c5fc07d40
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-02-23 15:47:31 +0100

    Add DictionaryConverter tests

---
+++ WebSosync.Tests/Converters/DictionaryConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace WebSosync.Converters.Tests
+{
+    public class DictionaryConverterTests
+    {
+        public static IEnumerable<object?[]> ConvertWorksData
+        {
+            get
+            {
+                yield return new object?[] { "{\"k\": \"v\"}", new Dictionary<string, string>() { ["k"] = "v" } };
+                yield return new object?[] { "{\"k1\": \"v1\", \"k2\": \"v2\"}", new Dictionary<string, string>() { ["k1"] = "v1", ["k2"] = "v2" } };
+                yield return new object?[] { "{\"k\": 1}", new Dictionary<string, string>() { ["k"] = "1" } };
+                yield return new object?[] { "{\"k\": true}", new Dictionary<string, string>() { ["k"] = "True" } };
+                yield return new object?[] { "{\"k\": false}", new Dictionary<string, string>() { ["k"] = "False" } };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ConvertWorksData))]
+        public void ConvertWorks(string json, Dictionary<string, string> expected)
+        {
+            var converter = new DictionaryConverter();
+            var options = new JsonSerializerOptions();
+            var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(json));
+            reader.Read();
+
+            var actual = converter.Read(ref reader, typeof(Dictionary<string, string>), options);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
