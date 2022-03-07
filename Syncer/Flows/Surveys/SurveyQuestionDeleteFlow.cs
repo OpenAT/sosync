e0d5187ad8e2fd61706ea25707732cb090e4198d
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-03-07 18:12:42 +0100

    Add survey.question sync and delete flows

---
+++ Syncer/Flows/Surveys/SurveyQuestionDeleteFlow.cs
@@ -0,0 +1,29 @@
+﻿using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Services;
+using System;
+using WebSosync.Data;
+
+namespace Syncer.Flows.Surveys
+{
+    [StudioModel(Name = "dbo.xFragebogenFrage")]
+    [OnlineModel(Name = "survey.question")]
+    public class SurveyQuestionDeleteFlow
+        : DeleteSyncFlow
+    {
+        public SurveyQuestionDeleteFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            throw new NotSupportedException($"{StudioModelName} cannot be synced to {SosyncSystem.FSOnline.Value}");
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleDeleteInStudio<dboxFragebogenFrage>(onlineID);
+        }
+    }
+}
