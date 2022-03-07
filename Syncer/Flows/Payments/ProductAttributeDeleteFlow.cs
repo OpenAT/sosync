fcefbbe87d6e1119a1b029f5036381eabf9bbfdd
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-03-07 14:34:21 +0100

    Add product.attribute sync and delete flow

---
+++ Syncer/Flows/Payments/ProductAttributeDeleteFlow.cs
@@ -0,0 +1,30 @@
+﻿using DaDi.Odoo.Models.Payments;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Services;
+using System;
+using WebSosync.Data;
+
+namespace Syncer.Flows.Payments
+{
+    [StudioModel(Name = "fson.product_attribute")]
+    [OnlineModel(Name = "product.attribute")]
+    public class ProductAttributeDeleteFlow
+        : DeleteSyncFlow
+    {
+        public ProductAttributeDeleteFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            throw new NotSupportedException($"{StudioModelName} cannot be synced to {SosyncSystem.FSOnline.Value}");
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleDeleteInStudio<fsonproduct_attribute>(onlineID);
+        }
+    }
+}
---
+++ Syncer/Flows/Payments/ProductAttributeFlow.cs
@@ -0,0 +1,46 @@
+﻿using DaDi.Odoo.Models.Payments;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Models;
+using Syncer.Services;
+using System;
+using WebSosync.Data;
+
+namespace Syncer.Flows.Payments
+{
+    [StudioModel(Name = "fson.product_attribute")]
+    [OnlineModel(Name = "product.attribute")]
+    public class ProductAttributeFlow
+        : ReplicateSyncFlow
+    {
+        public ProductAttributeFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override ModelInfo GetStudioInfo(int studioID)
+        {
+            return GetDefaultStudioModelInfo<fsonproduct_attribute>(studioID);
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            throw new NotSupportedException($"{StudioModelName} cannot be synced to {SosyncSystem.FSOnline.Value}");
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleTransformToStudio<productAttribute, fsonproduct_attribute>(
+                onlineID,
+                action,
+                studio => studio.product_attributeID,
+                (online, studio) =>
+                {
+                    studio.name = online.name;
+                    studio.display_name = online.display_name;
+                    studio.fso_write_date = online.write_date;
+                    studio.fso_create_date = online.create_date;
+                });
+        }
+    }
+}
