c3ff296ca893ca20de8e341288575f4f83f9097a
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-03-07 16:22:05 +0100

    Add product.attribute.price sync and delete flow

---
+++ Syncer/Flows/Payments/ProductAttributePriceFlow.cs
@@ -0,0 +1,72 @@
+﻿using DaDi.Odoo.Models.Payments;
+using dadi_data.Models;
+using Syncer.Attributes;
+using Syncer.Enumerations;
+using Syncer.Models;
+using Syncer.Services;
+using System;
+using WebSosync.Data;
+using WebSosync.Data.Constants;
+
+namespace Syncer.Flows.Payments
+{
+    [StudioModel(Name = "fson.product_attribute_price")]
+    [OnlineModel(Name = "product.attribute.price")]
+    public class ProductAttributePriceFlow
+        : ReplicateSyncFlow
+    {
+        public ProductAttributePriceFlow(SyncServiceCollection svc)
+            : base(svc)
+        { }
+
+        protected override ModelInfo GetStudioInfo(int studioID)
+        {
+            return GetDefaultStudioModelInfo<fsonproduct_attribute_price>(studioID);
+        }
+
+        protected override void SetupOnlineToStudioChildJobs(int onlineID)
+        {
+            var model = Svc.OdooService.Client.GetModel<productAttributePrice>(OnlineModelName, onlineID);
+
+            RequestChildJob(SosyncSystem.FSOnline, "product.attribute.value", Convert.ToInt32(model.value_id[0]), SosyncJobSourceType.Default);
+            RequestChildJob(SosyncSystem.FSOnline, "product.template", Convert.ToInt32(model.product_tmpl_id[0]), SosyncJobSourceType.Default);
+
+            base.SetupOnlineToStudioChildJobs(onlineID);
+        }
+
+        protected override void TransformToOnline(int studioID, TransformType action)
+        {
+            throw new NotSupportedException($"{StudioModelName} cannot be synced to {SosyncSystem.FSOnline.Value}");
+        }
+
+        protected override void TransformToStudio(int onlineID, TransformType action)
+        {
+            SimpleTransformToStudio<productAttributePrice, fsonproduct_attribute_price>(
+                onlineID,
+                action,
+                studio => studio.product_attribute_priceID,
+                (online, studio) =>
+                {
+                    var valueID = GetStudioIDFromOnlineReference(
+                        "product.attribute.value",
+                        online,
+                        x => x.value_id,
+                        true);
+
+                    var templateID = GetStudioIDFromOnlineReference(
+                        "product.template",
+                        online,
+                        x => x.product_tmpl_id,
+                        true);
+
+                    studio.product_attribute_valueID = valueID;
+                    studio.product_templateID = templateID;
+
+                    studio.display_name = online.display_name;
+                    studio.price_extra = online.price_extra;
+                    studio.fso_write_date = online.write_date;
+                    studio.fso_create_date = online.create_date;
+                });
+        }
+    }
+}
