ada1da20d9819c9ba5255f4e079b1b0053d04f4f
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2024-01-16 17:09:45 +0100

    Implement batch initial for online token sync

--- WebSosync/Services/TokenBatchService.cs
+++ WebSosync/Services/TokenBatchService.cs
@@ -5,6 +5,7 @@ using Dapper;
 using Microsoft.Extensions.Logging;
 using Syncer.Services;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -23,8 +24,7 @@ public class TokenBatchService
     private readonly SosyncOptions _options;
 
     public TokenBatchService(ILogger<TokenBatchService> logger, MdbService mdbService, FsoDataServiceFactory fsoDataFactory, SosyncOptions options)
-#warning TODO: Use 1 Minute!
-        : base(TimeSpan.FromSeconds(5), logger)
+        : base(TimeSpan.FromMinutes(1), logger)
     {
         _logger = logger;
         _mdbService = mdbService;
@@ -37,43 +37,32 @@ public class TokenBatchService
         using var mdb = _mdbService.GetDataService<dboAktionOnlineToken>();
         using var fso = _fsoDataFactory.Create();
 
-        /*
-         Child job hierarchy: OnlineToken > Person > zVerzeichnis
+        MdbTokenDto[] tokens;
+        do
+        {
+            tokens = await mdb.GetUnsynchronizedOnlineTokensAsync(_options.Token_Batch_Size);
 
-         Before mapping data
-            MSSQL: last_sync_version = sosync_write_date (if write_date is different)
-            MSSQL: sosync_fso_id = OdooID (if fso_id is null)
+            var s = Stopwatch.StartNew();
+            _logger.LogInformation("Token batch, synchronizing {tokenCount} tokens.", tokens.Length);
 
-         Mapping
-          
-            // Token specific fields
-            online.Add("name", studio.Name);
-            online.Add("partner_id", partner_id);
-            online.Add("expiration_date", studio.Ablaufdatum);
-            online.Add("fs_origin", studio.FsOrigin);
-            online.Add("last_datetime_of_use", studio.LetzteBenutzungAmUm);
-            online.Add("first_datetime_of_use", studio.ErsteBenutzungAmUm);
-            online.Add("number_of_checks", studio.AnzahlÜberprüfungen);
+            UpdateDefaults(tokens);
+            await fso.StoreTokensAsync(tokens);
+            await mdb.MarkTokensSynchronizedAsync(tokens);
+            await Task.Delay(1000, stoppingToken);
 
-            // General sosync fields (create + update)
-            online.Add("sosync_write_date", (studioModel.sosync_write_date ?? studioModel.write_date));
-            online.Add("frst_write_date", Treat2000DateAsNull(studioModel.write_date));
-            online.Add("sosync_synced_version", studioModel.last_sync_version);
-            
-            // General sosync fields (create only)
-            online.Add("frst_create_date", Treat2000DateAsNull(studioModel.create_date));
-            online.Add("sosync_fs_id", getStudioIdentity(studioModel));
+            s.Stop();
+            _logger.LogInformation("Token batch done in {milliseconds}ms.", s.ElapsedMilliseconds);
 
-         After create:
-        
-            MSSQL: sosync_fso_id
+        } while (tokens.Length > 0 && !stoppingToken.IsCancellationRequested);
+    }
 
-
-         Check if token exists - by name!
-         */
-
-        var tokens = await mdb.GetUnsynchronizedOnlineTokensAsync(_options.Token_Batch_Size);
-        var dummy = await fso.Connection.ExecuteScalarAsync<int>("select count(*) from res_partner;");
-
+    private void UpdateDefaults(MdbTokenDto[] tokens)
+    {
+        foreach (var token in tokens)
+        {
+            token.write_date = token.create_date;
+            token.sosync_write_date = token.create_date;
+            token.last_sync_version = token.create_date;
+        }
     }
 }
