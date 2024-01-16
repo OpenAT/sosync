68f3b87c3d1ff0341dfd04800a9b8c9b2a16b032
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2024-01-16 08:07:12 +0100

    Add data service and factor for direct fso db access

---
+++ Data/FsoDataService.cs
@@ -0,0 +1,28 @@
+﻿using Npgsql;
+using System;
+using System.Data.Common;
+using WebSosync.Data.Helpers;
+using WebSosync.Data.Models;
+
+namespace WebSosync.Data;
+
+public class FsoDataService
+    : IDisposable
+{
+    private readonly NpgsqlConnection _con;
+
+    public DbConnection Connection
+    {
+        get { return _con; }
+    }
+
+    public FsoDataService(string conStr)
+    {
+        _con = new NpgsqlConnection(conStr);
+    }
+
+    public void Dispose()
+    {
+        _con.Dispose();
+    }
+}
---
+++ Data/FsoDataServiceFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using WebSosync.Data.Helpers;
+using WebSosync.Data.Models;
+
+namespace WebSosync.Data;
+
+public class FsoDataServiceFactory
+{
+    private readonly IServiceProvider _services;
+    private readonly SosyncOptions _config;
+
+    public FsoDataServiceFactory(IServiceProvider services, SosyncOptions options)
+    {
+        _services = services;
+        _config = options;
+    }
+
+    public FsoDataService Create()
+    {
+        var conStr = ConnectionHelper.GetPostgresConnectionString(
+                _config.Online_DB_Host,
+                _config.Online_DB_Port,
+                _config.Online_DB_Name,
+                _config.Online_DB_User,
+                _config.Online_DB_User_PW);
+
+        return ActivatorUtilities.CreateInstance<FsoDataService>(_services, conStr);
+    }
+}
